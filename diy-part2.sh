876a0dbfb9319799ed610008a388ca5cd41b671f
167Day <54616567+cuphej@users.noreply.github.com>
2023-08-12 12:38:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,5 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/172.16.1.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/172.16.1.1/g' package/base-files/files/bin/config_generate
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
